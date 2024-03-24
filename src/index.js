7a0a8c0166d605c55c1cdd65b704e5ecd6ebd17c
lilllyyyyyyyy <lillyterzic@gmail.com>
2024-03-24 15:45:39 +0100

    added basic search engine features, no api

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function searchCity (event) {
+    event.preventDefault ();
+    let searchInput = document.querySelector("#search-form-input"); 
+    let cityElement = document.querySelector ("#city"); 
+    cityElement.innerHTML = searchInput.value; 
+
+
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchCity);
